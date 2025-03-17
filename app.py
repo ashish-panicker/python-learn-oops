1c15bd7bac2e2a94a8364689a676c09a8f629c57
ashish-panicker <ashish.s.panicker@proton.me>
2025-03-17 10:07:06 +0530

    Created OOPS python

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+OOPS - Object Oriented Programming System
+Key concepts here are classess and objects, scalable, reusable, modular
+
+Learning Management - Wipro
+Wipro hires new employees and ensure that they are trained according the needs of the organization.
+For this they create a comprehensive training programs that span over multiple weeks. 
+Each program is comprised of a series of related technologies which are taught by experinced senior mentors. 
+Evey trainee has to undergo this rigorous program before they are inducted into projects. 
+Trainees are grouped into batches for easier management and delivery. 
+Trainees will have to undergo stringent assessments and evaluations and the results of these determine 
+their completion outcome.
+
+Identify the classess, their properties, their relationships in this problem statement.
+
+"""
