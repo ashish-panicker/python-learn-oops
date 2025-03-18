347b05ba544479f7ad6dd1e7ba9a6b4684d4b50b
ashish-panicker <ashish.s.panicker@proton.me>
2025-03-18 12:14:55 +0530

    isinstance & issubclass

--- multiple_inheritance/sample.py
+++ multiple_inheritance/sample.py
@@ -28,3 +28,8 @@ print(d.method()) # Output of this statement?
 D.method() -> B.method() -> C.method() -> A.method()
 A's method then C's method then B's method then D's method
 """
+
+print(f"d instance of class D, {isinstance(d, D)}")
+print(f"is d subclass of B, {issubclass(D, B)}")
+print(f"is d subclass of C, {issubclass(D, C)}")
+print(f"is d subclass of A, {issubclass(D, A)}")
